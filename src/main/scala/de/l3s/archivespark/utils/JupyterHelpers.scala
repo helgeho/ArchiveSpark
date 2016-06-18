f5b1a416ddc3329de2db3e13cb6b8bd420a5a71f
Helge <holzmann@L3S.de>
2016-06-18 09:38:28 +0200

    Add Jupyter helpers

---
+++ src/main/scala/de/l3s/archivespark/utils/JupyterHelpers.scala
@@ -0,0 +1,68 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015-2016 Helge Holzmann (L3S) and Vinay Goel (Internet Archive)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package de.l3s.archivespark.utils
+
+import org.apache.spark.rdd.RDD
+
+import scala.util.Random
+
+object JupyterHelpers {
+  def printHtml(html: String) = Left(Map("text/html" -> html))
+
+  def init() = {
+    printHtml("""
+      <script type="text/javascript" src="https://rawgit.com/caldwell/renderjson/master/renderjson.js"></script>
+      <style type="text/css">
+        .renderjson .disclosure    { color: black; font-weight: bold; text-decoration: none; }
+        .renderjson .syntax        { color: black; }
+        .renderjson .string        { color: darkgreen; }
+        .renderjson .number        { color: purple; }
+        .renderjson .boolean       { color: red; }
+        .renderjson .key           { color: darkblue; }
+        .renderjson .keyword       { color: orange; }
+      </style>
+    """)
+  }
+
+  def printJson(json: String, showToLevel: Int = 1) = {
+    val id = Random.nextInt(Int.MaxValue)
+    printHtml(s"""
+      <div id="$id"></div>
+      <script>
+        (function () {
+          var render = renderjson.set_show_to_level($showToLevel).set_max_string_length(200);
+          document.getElementById("$id").appendChild(
+            render(${json.replace("<", "\\u003C").replace(">", "\\u003E")})
+          );
+        })();
+      </script>
+    """)
+  }
+
+  def peek[T <: JsonConvertible](rdd: RDD[T]) = {
+    val json = rdd.take(1).headOption.map(_.toJsonString(false)).getOrElse("")
+    printJson(json)
+  }
+}
