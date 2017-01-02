f3b2231b47c8d4cffe9f89a1151f482138bcf9e7
Helge Holzmann <helgeho@invelop.de>
2017-01-02 17:17:05 +0100

    update copyright year

--- src/main/scala/de/l3s/archivespark/enrich/dataloads/ByteContentLoad.scala
+++ src/main/scala/de/l3s/archivespark/enrich/dataloads/ByteContentLoad.scala
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2016 Helge Holzmann (L3S) and Vinay Goel (Internet Archive)
+ * Copyright (c) 2015-2017 Helge Holzmann (L3S) and Vinay Goel (Internet Archive)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
